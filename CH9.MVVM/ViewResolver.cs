4cefe15c7e771d7eecd62186054d35bf1db3ed5e
emeraldpiggy <george@expert4.com.au>
2016-10-11 23:22:04 +1100

    add resolver

--- CH9.MVVM/ViewResolver.cs
+++ CH9.MVVM/ViewResolver.cs
@@ -1,12 +1,175 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Caliburn.Micro;
 
 namespace CH9.MVVM
 {
-    class ViewResolver
+    public class ViewResolver : IViewResolver
     {
+        private static bool _uiAutomationIDSupport;
+
+        public static void Initialise()
+        {
+            ViewLocator.ContextSeparator = string.Empty;
+
+            //Add transform rules for ViewModel - PropertyEditor\{base}Property
+            ViewLocator.NameTransformer.AddRule
+                (
+                    @"(?<nsbefore>([A-Za-z_]\w*\.)*)?(?<nsvm>ViewModels\.)(?<nsafter>[A-Za-z_]\w*\.)*(?<basename>[A-Za-z_]\w*)(?<suffix>ViewModel$)",
+                    @"${nsbefore}Views.PropertyEditors.${nsafter}${basename}View",
+                    @"(([A-Za-z_]\w*\.)*)?ViewModels\.([A-Za-z_]\w*\.)*[A-Za-z_]\w*ViewModel$"
+                );
+
+            ViewLocator.NameTransformer.AddRule
+                (
+                    @"(?<nsbefore>([A-Za-z_]\w*\.)*)?(?<nsvm>ViewModels\.)(?<nsafter>[A-Za-z_]\w*\.)*(?<basename>[A-Za-z_]\w*)(?<suffix>ViewModel$)",
+                    @"${nsbefore}Views.PropertyEditors.${basename}View",
+                    @"(([A-Za-z_]\w*\.)*)?ViewModels\.([A-Za-z_]\w*\.)*[A-Za-z_]\w*ViewModel$"
+                );
+
+            //Add convention transform rules for EditorModel - Editor
+            ViewLocator.NameTransformer.AddRule(
+                    @"(?<nsbefore>([A-Za-z_]\w*\.)*)(?<subns>ViewModels.EditorModels\.)(?<nsafter>([A-Za-z_]\w*\.)*)(?<basename>[A-Za-z_]\w*)(?<suffix>EditorModel)$",
+                    @"${nsbefore}Views.Editors.${nsafter}${basename}Editor"
+                );
+
+            ViewLocator.NameTransformer.AddRule(
+                    @"(?<origns>\.)(?<basename>[A-Za-z_]\w*)(?<suffix>EditorModel)$",
+                    @".${basename}Editor"
+                );
+
+            _uiAutomationIDSupport = true;
+        }
+        public static IViewResolver View
+        {
+            get
+            {
+                return new ViewResolver();
+            }
+        }
+
+        internal static UIElement LocateForModelType(Type modelType, object model, object context)
+        {
+            string viewTypeName = modelType.FullName;
+            if (Execute.InDesignMode)
+            {
+                viewTypeName = ViewLocator.ModifyModelTypeAtDesignTime(viewTypeName);
+            }
+
+            var viewType = ViewLocator.LocateTypeForModelType(modelType, null, context);
+
+            UIElement view;
+
+            if (viewType != null && typeof(System.Windows.Forms.Control).IsAssignableFrom(viewType))
+            {
+                var localCtrl = Enumerable.FirstOrDefault(CaliburnIoC.GetAllInstances(viewType)) as System.Windows.Forms.Control;
+                if (localCtrl == null)
+                {
+                    localCtrl = (System.Windows.Forms.Control)Activator.CreateInstance(viewType);
+                }
+
+                var dockPanel = new DockPanel { LastChildFill = true };
+
+                var host = new WindowsFormsHost
+                {
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch
+                };
+
+
+                DockPanel.SetDock(host, Dock.Top);
+                var dataResource = new DataResource { BindingTarget = new Binding() };
+                host.Resources.Add("WndFormBindingResource", dataResource);
+                if (localCtrl is IDataContext)
+                {
+                    var drBinding = new DataResourceBindingExtension();
+                    drBinding.DataResource = dataResource;
+                    var drBindingHelper = new WindowFormsDataResouceBindingHelper(localCtrl);
+                    drBinding.ProvideValue(drBindingHelper);
+                    ((IDataContext)localCtrl).DataContext = model;
+                }
+                host.Child = localCtrl;
+                ViewLocator.InitializeComponent(localCtrl);
+                dockPanel.Children.Add(host);
+                view = dockPanel;
+            }
+            else
+            {
+
+                if (viewType == null)
+                {
+                    return new TextBlock { Text = string.Format("{0} not found.", viewTypeName) };
+                }
+
+                view = ViewLocator.GetOrCreateViewType(viewType);
+            }
+
+
+            return view;
+
+        }
+        public UIElement Resolve(object context)
+        {
+            return LocateForModelType(context.GetType(), context, null);
+        }
+
+
+        public Type ResolveType(object context)
+        {
+            if (context == null)
+                throw new NoNullAllowedException();
+
+            var t = ViewLocator.LocateTypeForModelType(context.GetType(), null, null);
+
+            return t;
+        }
+
+        public void Bind(FrameworkElement view, object viewModel)
+        {
+            view.DataContext = viewModel;
+            BindingApplied(viewModel, view);
+        }
+
+        public UIElement ResolveAndBind(object context)
+        {
+            var view = Resolve(context);
+            if (view != null && view is FrameworkElement)
+            {
+                Bind((FrameworkElement)view, context);
+            }
+            return view;
+        }
+
+
+        public static void Bind(object model, UIElement view, object context)
+        {
+            ViewModelBinder.Bind(model, view, context);
+            BindingApplied(model, view, context);
+        }
+
+        private static void BindingApplied(object model, UIElement view, object context = null)
+        {
+            var value = context ?? model;
+
+            if (string.IsNullOrEmpty(AutomationProperties.GetAutomationId(view)))
+            {
+                BindingOperations.SetBinding(view, AutomationProperties.AutomationIdProperty, new Binding("Title") { Source = value, Mode = BindingMode.OneWay });
+            }
+        }
+    }
+
+
+    public interface IVisualState : INotifyPropertyChanged
+    {
+        string VisualState { get; set; }
     }
 }
