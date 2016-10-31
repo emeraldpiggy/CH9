27f0e6e727fce242115dc20ba8b1587c886ef3af
Georg236 <George.zhu@towerswatson.com>
2016-10-31 19:19:45 +1100

    fix mvvm framework

---
+++ CH9.Shell/ShellView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CH9.Shell
+{
+    /// <summary>
+    /// Interaction logic for ShellView.xaml
+    /// </summary>
+    public partial class ShellView
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
