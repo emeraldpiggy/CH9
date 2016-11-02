3930a923f89e7c8fffe86e758e642a96b8d737ed
Georg236 <George.zhu@towerswatson.com>
2016-11-02 17:21:42 +1100

    remove signal R

--- CH9.Repository/Entity/CleaningHouseModel.cs
+++ CH9.Repository/Entity/CleaningHouseModel.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using CH9.Framework;
 
 namespace CH9.Repository.Entity
