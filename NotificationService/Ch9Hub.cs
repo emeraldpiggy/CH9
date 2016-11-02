8db8bfe1252eecf0ab0706544e8539bf77e4a365
Georg236 <George.zhu@towerswatson.com>
2016-11-02 17:21:19 +1100

    remove signal R

--- NotificationService/Ch9Hub.cs
+++ NotificationService/Ch9Hub.cs
@@ -11,14 +11,14 @@ namespace CH9.NotificationService
         private void StartPushingService()
         {
             var address = "http://localhost:8808";
-            _signalR = WebApp.Start<Startup>(address);
+            //_signalR = WebApp.Start<Startup>(address);
         }
 
 
         protected void StopServiceHosts()
         {
-            if (_signalR != null)
-                _signalR.Dispose();
+            //if (_signalR != null)
+            //    _signalR.Dispose();
         }
 
     }
